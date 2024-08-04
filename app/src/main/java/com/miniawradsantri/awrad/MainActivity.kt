91011200be9b22a398ef270981261a5db5e8d0dd
nhnf <nhnf198@gmail.com>
2024-08-04 23:34:19 +0700

    navbar belum jadi

--- app/src/main/java/com/miniawradsantri/awrad/MainActivity.kt
+++ app/src/main/java/com/miniawradsantri/awrad/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.miniawradsantri.awrad
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.miniawradsantri.awrad.artikel.ArtikelActivity
+import com.miniawradsantri.awrad.databinding.ActivityMainBinding
+import com.miniawradsantri.awrad.tentang.TentangActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navigationListener()
+
+    }
+
+    private fun navigationListener() {
+        binding.navigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.nav_artikel -> {
+                    Intent(this, ArtikelActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.nav_tentang -> {
+                    Intent(this, TentangActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            false
         }
     }
 }
---
+++ app/src/main/java/com/miniawradsantri/awrad/artikel/ArtikelActivity.kt
@@ -0,0 +1,21 @@
+package com.miniawradsantri.awrad.artikel
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.miniawradsantri.awrad.R
+
+class ArtikelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_artikel)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
