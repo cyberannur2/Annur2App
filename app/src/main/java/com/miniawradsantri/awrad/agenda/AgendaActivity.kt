20f369afb5beb024fc218ad00ae492d5f297e2cb
nhnf <nhnf198@gmail.com>
2024-08-04 20:23:24 +0700

    belum bisa scroll

---
+++ app/src/main/java/com/miniawradsantri/awrad/agenda/AgendaActivity.kt
@@ -0,0 +1,21 @@
+package com.miniawradsantri.awrad.agenda
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.miniawradsantri.awrad.R
+
+class AgendaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_agenda)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
